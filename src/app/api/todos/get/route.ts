79d8a0ae19a787c0771d9eedb21c5038dbd2254d
uwishema11 <uwishemaceline4@gmail.com>
2024-07-30 14:19:32 +0530

    Fix get api functionalities

--- src/app/api/todos/get/route.ts
+++ src/app/api/todos/get/route.ts
@@ -1,21 +1,11 @@
 import { db } from '@/db';
 import { todos } from '@/db/schema';
-import { eq } from 'drizzle-orm';
-import { auth } from '@/auth';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(req: NextRequest) {
   try {
-    const session = await auth();
-    if (!session?.user) {
-      return NextResponse.json(
-        { message: 'Not Authenticated' },
-        { status: 401 },
-      );
-    }
 
-    const userId = session?.user.id as string;
-    const query = await db.select().from(todos).where(eq(todos.id, userId));
+    const query = await db.select().from(todos);
 
     return NextResponse.json(query);
   } catch (error) {
