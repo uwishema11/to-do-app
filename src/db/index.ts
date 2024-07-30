8b357839d35d5185ad45f9fd090107aa7c3ee478
uwishema11 <uwishemaceline4@gmail.com>
2024-07-30 13:11:49 +0200

    fixing the neon connection

--- src/db/index.ts
+++ src/db/index.ts
@@ -3,6 +3,9 @@ import { drizzle } from 'drizzle-orm/neon-http';
 import * as schema from './schema';
 
 let sql;
-const DB_UL = process.env.DATABASE_URL as string;
-sql = neon(DB_UL);
+const DB_URL = process.env.DATABASE_URL!;
+if (!DB_URL) {
+  throw new Error('DATABASE_URL is not defined');
+}
+sql = neon(DB_URL);
 export const db = drizzle(sql, { schema });
